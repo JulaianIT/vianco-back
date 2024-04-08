0720c197f6cc6a03d1d960157ed451e4a8a7fb29
julian <amayacarlos898@gmail.com>
2024-04-08 12:25:50 -0500

    descarga png

--- src/public/consulta.js
+++ src/public/consulta.js
@@ -79,60 +79,78 @@ document.addEventListener('DOMContentLoaded', () => {
             const fotoURL = vehiculo.fotoURL; // Suponiendo que recibes la URL base64 del servidor
         
             infoVehiculoDiv.innerHTML = `
-            <h2>INFORMACION</h2>
-            <div class="conductor-image">
-                <img src="${fotoURL}" alt="Foto del Conductor" style="width: 100%; height: 100%; object-fit: cover;">
+            <div class="png">
+
+            <div class="containerr">
+            <div class="arriba">
+            <img id="logoo" src="/imagenes/logo vianco.png" alt="Logo">
+            <div class="contenidoo">
+                <h3>TRASPORTE</h3>
+                <h3>HOJA DE VIDA VEHICULO</h3>
             </div>
-           <p><strong>PLACA:</strong> ${vehiculo.Placa}</p>
-           <p><strong>BASE:</strong> ${vehiculo.Base}</p>
-           <p><strong>CONDUCTOR:</strong> ${vehiculo.Conductor}</p>
-           <p><strong>N° MOVIL:</strong> ${vehiculo.No_movil}</p>
-           <p><strong>MATRICULA:</strong> ${vehiculo.Matricula}</p>
-           <p><strong>MARCA:</strong> ${vehiculo.Marca}</p>
-           <p><strong>LINEA:</strong> ${vehiculo.Linea}</p>
-           <p><strong>MODELO:</strong> ${vehiculo.Modelo}</p>
-           <p><strong>N° MOTOR:</strong> ${vehiculo.Numero_motor}</p>
-           <p><strong>N° CHASIS:</strong> ${vehiculo.Numero_chasis}</p>
-           <p><strong>CLASE:</strong> ${vehiculo.Clase_vehiculo	}</p>
-         
-           <p><strong>N° DE PUESTOS:</strong> ${vehiculo.Num_puestos}</p>
-           <p><strong>N° DE PUERTA:</strong> ${vehiculo.Puertas}</p>
-           <p><strong>N° DE EJES:</strong> ${vehiculo.Num_ejes}</p>
-           <p><strong>CILINDRAJE:</strong> ${vehiculo.Cilindraje}</p>
-           <p><strong>COLOR:</strong> ${vehiculo.Color}</p>
-           <p><strong>COMBUSTIBLE:</strong> ${vehiculo.Combustible}</p>
-           <p><strong>CARROCERIA:</strong> ${vehiculo.Carroceria}</p>
-           <p><strong>FECHA MATRICULA:</strong> ${vehiculo.Fecha_matricula}</p>
-           <hr>
+            <div class="info">
+                <p><strong>PLACA:</strong> ${vehiculo.Placa}</p>
+                <p><strong>BASE:</strong> ${vehiculo.Base}</p>
+                <p><strong>CONDUCTOR:</strong> ${vehiculo.Conductor}</p>
+            </div>
+        </div>
+            </div>
+            <div class="conductor-image">
+                <img class="image"  src="${fotoURL}" alt="Foto del Conductor" >
+            </div>
+            <h2>INFORMACION VEHICULO</h2>
 
-           <h2>Documentos reglamentarios</h2>
-           <p><strong>N°SOAT:</strong> ${vehiculo.Num_soat}</p>
-           
-           <p><strong>COMPANIA ASEGURADORA:</strong> ${vehiculo.Entidad}</p>
-         
-           <p><strong>FECHA VIGENCIA :</strong> ${vehiculo.Fecha_vigencia_soat}</p>
-           <p><strong>N° TECNOMECANICA:</strong> ${vehiculo.Num_tecnomecanica}</p>
-           <p><strong>CDA:</strong> ${vehiculo.Cda}</p>
-           <p><strong>FECHA INICIO :</strong> ${vehiculo.Fecha_inicio_tecnomecanica}</p>
-           <p><strong>FECHA VIGENCIA:</strong> ${vehiculo.Fecha_vigencia}</p>
-           <p><strong>N° POLIZAS:</strong> ${vehiculo.Num_polizas_rcc_rce}</p> 
-           <p><strong>COMPAÑIA ASEGURADORA:</strong> ${vehiculo.Compania_aseguradora}</p> 
+            <div class="contenedor-columnas">
+            <div class="columna">
+                <p><strong>N° MOVIL:</strong> ${vehiculo.No_movil}</p>
+                <p><strong>MATRICULA:</strong> ${vehiculo.Matricula}</p>
+                <p><strong>MARCA:</strong> ${vehiculo.Marca}</p>
+                <p><strong>LINEA:</strong> ${vehiculo.Linea}</p>
+                <p><strong>MODELO:</strong> ${vehiculo.Modelo}</p>
+                <p><strong>N° MOTOR:</strong> ${vehiculo.Numero_motor}</p>
+                <p><strong>N° CHASIS:</strong> ${vehiculo.Numero_chasis}</p>
+                <p><strong>CLASE:</strong> ${vehiculo.Clase_vehiculo}</p>
+            </div>
+            <div class="columna">
+            <p><strong>N° DE PUESTOS:</strong> ${vehiculo.Num_puestos}</p>
 
+                <p><strong>N° DE PUERTA:</strong> ${vehiculo.Puertas}</p>
+                <p><strong>N° DE EJES:</strong> ${vehiculo.Num_ejes}</p>
+                <p><strong>CILINDRAJE:</strong> ${vehiculo.Cilindraje}</p>
+                <p><strong>COLOR:</strong> ${vehiculo.Color}</p>
+                <p><strong>COMBUSTIBLE:</strong> ${vehiculo.Combustible}</p>
+                <p><strong>CARROCERIA:</strong> ${vehiculo.Carroceria}</p>
+                <p><strong>FECHA MATRICULA:</strong> ${vehiculo.Fecha_matricula}</p>
+            </div>
+        </div>
+        <h2>DOCUMENTO REGLAMENTARIOS</h2>
 
-
-           <p><strong>FECHA VIGENCIA:</strong> ${vehiculo.Vigencia_polizas}</p>
-           <p><strong>N° TARJETA OPERACION:</strong> ${vehiculo.Num_tarjeta_operacion}</p>
-           <p><strong>EMPRESA AFILIACION:</strong> ${vehiculo.Empresa_afiliacion}</p>
-           <p><strong>FECHA VIGENCIA:</strong> ${vehiculo.Fecha_final_operacion}</p>
-           <p><strong>N° PREVENTIVA :</strong> ${vehiculo.Num_preventiva_1}</p>
-           <p><strong>CDA PREVENTIVA:</strong> ${vehiculo.Cda_preventiva}</p>
-           <p><strong>FECHA INICIAL:</strong> ${vehiculo.Fecha_inicial_preventiva_1}</p>
-           <p><strong>FECHA VIGENCIA:</strong> ${vehiculo.Fecha_final_preventiva_1}</p>
-           
-            
-           <p><strong>N° DE CONVENIO:</strong> ${vehiculo.n_convenio}</p>
-           <p><strong>FECHA DE VIGENCIA:</strong> ${vehiculo.fecha_vigencia_convenio}</p>
-
+        <div class="contenedor-columnas">
+    <div class="columna">
+        <p><strong>N°SOAT:</strong> ${vehiculo.Num_soat}</p>
+        <p><strong>COMPANIA ASEGURADORA:</strong> ${vehiculo.Entidad}</p>
+        <p><strong>FECHA VIGENCIA:</strong> ${vehiculo.Fecha_vigencia_soat}</p>
+        <p><strong>N° TECNOMECANICA:</strong> ${vehiculo.Num_tecnomecanica}</p>
+        <p><strong>CDA:</strong> ${vehiculo.Cda}</p>
+        <p><strong>FECHA INICIO:</strong> ${vehiculo.Fecha_inicio_tecnomecanica}</p>
+        <p><strong>FECHA VIGENCIA:</strong> ${vehiculo.Fecha_vigencia}</p>
+        <p><strong>N° POLIZAS:</strong> ${vehiculo.Num_polizas_rcc_rce}</p>
+        <p><strong>COMPAÑIA ASEGURADORA:</strong> ${vehiculo.Compania_aseguradora}</p>
+        <p><strong>FECHA VIGENCIA:</strong> ${vehiculo.Vigencia_polizas}</p>
+    </div>
+    <div class="columna">
+       
+        <p><strong>N° TARJETA OPERACION:</strong> ${vehiculo.Num_tarjeta_operacion}</p>
+        <p><strong>EMPRESA AFILIACION:</strong> ${vehiculo.Empresa_afiliacion}</p>
+        <p><strong>FECHA VIGENCIA:</strong> ${vehiculo.Fecha_final_operacion}</p>
+        <p><strong>N° PREVENTIVA:</strong> ${vehiculo.Num_preventiva_1}</p>
+        <p><strong>CDA PREVENTIVA:</strong> ${vehiculo.Cda_preventiva}</p>
+        <p><strong>FECHA INICIAL:</strong> ${vehiculo.Fecha_inicial_preventiva_1}</p>
+        <p><strong>FECHA VIGENCIA:</strong> ${vehiculo.Fecha_final_preventiva_1}</p>
+        <p><strong>N° DE CONVENIO:</strong> ${vehiculo.n_convenio}</p>
+        <p><strong>FECHA DE VIGENCIA:</strong> ${vehiculo.fecha_vigencia_convenio}</p>
+    </div>
+</div>
 
            <hr>
 
@@ -148,12 +166,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-         
+           </div>
 
            <button id="editar-btn" class="btn editar-btn">Editar Información</button>
            <a href="/agregar-vehiculo" class="volver-btn">Agregar Nuevo Vehículo</a>
 
            <a href="/" class="volver-btn">Volver al Menú</a>
+
 
            `;
        
@@ -173,4 +192,5 @@ document.addEventListener('DOMContentLoaded', () => {
                 infoVehiculoDiv.textContent = 'Error al consultar el vehículo';
             });
         });
-    });
+        
+    })
